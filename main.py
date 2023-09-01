334cfd5b991a328a223cbfe2aaee9c64c9128b4e
LMingJian <1312635752@qq.com>
2023-09-01 17:09:36 +0800

    增加一个可用书源

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from novel_spider import Spider
 
 if __name__ == '__main__':
-    Spider(r'F:\PythonProject\WebDrive\geckodriver.exe', [], 3)
+    Spider(r'F:\PythonProject\WebDrive\geckodriver.exe', ['--headless'], 4)
